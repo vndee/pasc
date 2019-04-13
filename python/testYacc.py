3fa7d1260541647067b2b10c60b7dcf4539fb4a7
Duy Huynh <hvd.huynhduy@gmail.com>
2019-04-13 13:45:09 +0700

    change structure

--- python/testYacc.py
+++
@@ -1,9 +0,0 @@
-from .utils.yacc import  yacc
-from .utils.rules import *
-from .utils.semantic import *
-
-parser = yacc()
-f = open('./Lexical/Test05.txt', 'r')
-raw_input = f.read()
-result = parser.parse(raw_input)
-print(result)
